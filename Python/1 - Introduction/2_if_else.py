4e95be05b0d828f5cb803b31502f092ee5fe1e17
appositum <appositum@pm.me>
2023-11-06 19:41:11 -0300

    Python introduction

---
+++ Python/1 - Introduction/2_if_else.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    if n%2 == 0:
+        if n in range(2,6):
+            print("Not Weird")
+        elif n in range(6,21):
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
+    else:
+        print("Weird")
